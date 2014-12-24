da35f0b25a21d963ac2927a25569f09240e34f3e
mautematico <mauricio@navarromiranda.mx>
2014-12-24 15:59:50 -0600

    Agrego nuevas ideas

--- js/ideas.js
+++ js/ideas.js
@@ -1,5 +1,15 @@
-var antesdemorir_ideas = [
-  "Poner helado de vainilla en un frasco de mostaza y salir con él a la calle para comerlo mientras todos te observan.",
+var antesdemorir_ideas =
+[
+  "Volar una avioneta",
+  "Lanzarte en parapente",
+  "Aventarte del bungee",
+  "Sobrevivir una semana en la Amazonía",
+  "Invitar a comer a tu casa a una persona de la calle",
+  "Aprender a tocar una pieza en el piano",
+  "Leer un libro a un ciego",
   "Escalar el Monte Everest.",
-  "Hacer una parada de manos"
+  "Visitar la granja de Emily Rose.",
+  "Actuar de extra en una película",
+  "Hacer una parada de manos",
+  "Poner helado de vainilla en un frasco de mostaza y salir con él a la calle para comerlo mientras todos te observan."
 ]
