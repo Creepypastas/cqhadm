189895f6cfd22018b41faa37106de8c70d9f94b8
mautematico <mauricio@navarromiranda.mx>
2014-12-24 14:57:57 -0600

    Demosle un toque de azar a la frase inicial. :)

---
+++ js/ideas.js
@@ -0,0 +1,5 @@
+var antesdemorir_ideas = [
+  "Poner helado de vainilla en un frasco de mostaza y salir con él a la calle para comerlo mientras todos te observan.",
+  "Escalar el Monte Everest.",
+  "Hacer una parada de manos"
+]
