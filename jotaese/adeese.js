57e253bd10debcc33e67219ad254c56d2fd789a0
mautematico <mauricio@navarromiranda.mx>
2014-12-29 14:54:45 -0600

    admob

--- jotaese/adeese.js
+++ jotaese/adeese.js
@@ -4,10 +4,5 @@ if( /(android)/i.test(navigator.userAgent) ) {
 	    banner: 'ca-app-pub-4052984890763114/9754796058',
 	    interstitial: 'ca-app-pub-4052984890763114/6801329653'
 	};
-
-	document.addEventListener("DOMContentLoaded", function(event) {
-		if(typeof AdMob != "undefined") AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
-		if(typeof AdMob != "undefined") AdMob.showInterstitial();
-	});
 }
 
--- jotaese/cqhadm.js
+++ jotaese/cqhadm.js
@@ -46,4 +46,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
   
   document.getElementsByTagName('textarea')[0].onchange=cqhadm_modificado;
   
+	if( /(android)/i.test(navigator.userAgent) ) {
+		admobid = {
+		    banner: 'ca-app-pub-4052984890763114/9754796058',
+		    interstitial: 'ca-app-pub-4052984890763114/6801329653'
+		};
+		if(typeof AdMob != "undefined") AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
+		if(typeof AdMob != "undefined") AdMob.showInterstitial();
+	}
+
+
 });
